5a6ddb9c1465cd946e720cce70b03b324d93f1ad
Ahmad Kaif <heyahmad611@gmail.com>
2025-02-04 21:06:19 +0530

    .

--- server/src/config/connectDb.ts
+++ server/src/config/connectDb.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async (): Promise<void> => {
     try {
         await mongoose.connect(process.env.MONGO_URI as string);
+        // await mongoose.connect("mongodb+srv://ahmad:ahmad123@cluster0.bnpif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
